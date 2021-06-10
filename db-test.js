37d539e3014042b4b16cace669a826fd544bc37c
nesbita <ariananesbit@gmail.com>
2021-06-09 22:02:09 -0600

    added list route and links, 2nd API url, added redirect to details

--- db-test.js
+++ db-test.js
@@ -1,2 +1,7 @@
 const db = require('./models');
 
+let x = ""
+
+x = x.slice(2)
+console.log( x )
+
--- index.js
+++ index.js
@@ -11,10 +11,32 @@ app.use(express.static(__dirname + '/public/'))
 
 app.get('/', (req, res) => {
     res.render('index')
+});
+
+// redirect to /list page after clicking button on home page 
+app.post('/list', (req, res) => {
+    console.log(req.body.zipcode)
+  let apiUrl1 = `http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${req.body.zipcode}`;
+  axios.get(apiUrl1).then(apiResponse => {
+    let farmMarket = apiResponse.data.results;
+    res.render('list', { farmMarket });
+    // console.log('it worked! 🐸 ')
+  })
+});
+
+app.get('/details/:id', (req, res) => {
+  let apiUrl2 = `http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=${req.params.id}`;
+  axios.get(apiUrl2).then(apiResponse => {
+    console.log(apiResponse.data)
+    let farmMarketData = apiResponse.data;
+    res.render('details', { farmMarketData });
+  })
+    console.log('🐧')
 })
 
 app.use('/markets', require('./routes/markets'))
 
+
 app.listen(port, () => {
     console.log('...listening on', port );
-});
+})
